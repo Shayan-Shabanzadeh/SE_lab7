9df05f60bd30b9ecedb0dbe5395a06b2cae7eedd
a.rahimzadeh <a.rahimzadeh@emofid.com>
2023-12-25 21:08:13 +0330

    Add repository tests

---
+++ src/test/java/com/unittest/codecoverage/repository/PersonRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.unittest.codecoverage.repository;
+
+import com.unittest.codecoverage.models.Gender;
+import com.unittest.codecoverage.models.Person;
+import com.unittest.codecoverage.repositories.PersonRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonRepositoryTest {
+
+    @Test
+    public void testInsert_shouldInsertPerson() {
+        PersonRepository repository = new PersonRepository();
+        Person person = new Person();
+        person.setName("Name");
+        person.setAge(21);
+        person.setGender(Gender.M);
+
+        Person result = repository.insert(person);
+
+        assertNotNull(result);
+        assertEquals("Name", result.getName());
+        assertEquals(21, result.getAge());
+        assertEquals(Gender.M, result.getGender());
+    }
+
+    @Test
+    public void testUpdate_shouldUpdatePerson() {
+        PersonRepository repository = new PersonRepository();
+        Person person = new Person();
+        person.setName("Name");
+        person.setAge(21);
+        person.setGender(Gender.M);
+
+        repository.update(person);
+
+    }
+
+    @Test
+    public void testDelete_shouldDeletePerson() {
+        PersonRepository repository = new PersonRepository();
+        String name = "John";
+
+        repository.delete(name);
+
+    }
+
+    @Test
+    public void testGet_shouldReturnPerson() {
+        PersonRepository repository = new PersonRepository();
+        String name = "John";
+
+        Person result = repository.get(name);
+
+        assertNull(result);
+    }
+}
